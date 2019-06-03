60c43ec5e6e056926bec8221052d553ce658b5e5
lvs007 <zy.liang.5@163.com>
2019-06-03 17:10:56 +0800

    add the check point

--- src/main/java/org/tron/core/pbft/PbftMessageAction.java
+++ src/main/java/org/tron/core/pbft/PbftMessageAction.java
@@ -3,15 +3,22 @@ package org.tron.core.pbft;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.tron.common.overlay.message.Message;
+import org.tron.core.pbft.message.PbftBlockMessageCapsule;
 
 @Slf4j
 @Component
 public class PbftMessageAction {
 
+  private long checkPoint = 0;
+  private static int count = 1;
+
   public void action(Message message) {
     switch (message.getType()) {
       case PBFT_BLOCK_MSG: {
-
+        PbftBlockMessageCapsule blockMessage = (PbftBlockMessageCapsule) message;
+        if (blockMessage.getPbftMessage().getRawData().getBlockNum() - checkPoint >= count) {
+          checkPoint = blockMessage.getPbftMessage().getRawData().getBlockNum();
+        }
       }
       break;
       default:
