642a296f00906bc02a5dade9ab187ff1dd8161ee
Matt <yrp1990@gmail.com>
2019-07-24 12:06:44 +0800

    add some debug for futureGet

--- src/main/java/org/tron/core/services/interfaceOnSolidity/WalletOnSolidity.java
+++ src/main/java/org/tron/core/services/interfaceOnSolidity/WalletOnSolidity.java
@@ -18,16 +18,8 @@
 
 package org.tron.core.services.interfaceOnSolidity;
 
-import com.google.common.util.concurrent.ListenableFuture;
-import com.google.common.util.concurrent.ListeningExecutorService;
-import com.google.common.util.concurrent.MoreExecutors;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
-import lombok.Getter;
+import java.util.concurrent.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -39,9 +31,9 @@ import org.tron.core.db.Manager;
 @Component
 public class WalletOnSolidity {
 
-  private ListeningExecutorService executorService = MoreExecutors.listeningDecorator(
+  private ExecutorService executorService =
       Executors.newFixedThreadPool(Args.getInstance().getSolidityThreads(),
-          new ThreadFactoryBuilder().setNameFormat("WalletOnSolidity-%d").build()));
+          new ThreadFactoryBuilder().setNameFormat("WalletOnSolidity-%d").build());
 
   @Autowired
   private Manager dbManager;
@@ -49,45 +41,49 @@ public class WalletOnSolidity {
   private Wallet wallet;
 
   public <T> T futureGet(Callable<T> callable) {
-    ListenableFuture<T> future = executorService.submit(() -> {
+    Future<T> future = executorService.submit(() -> {
       try {
         dbManager.setMode(false);
         return callable.call();
       } catch (Exception e) {
-        logger.info("futureGet " + e.getMessage());
+        logger.info("futureGet " + e.getMessage(), e);
         return null;
       }
     });
 
     try {
-      return future.get(1000, TimeUnit.MILLISECONDS);
+      return future.get(10000, TimeUnit.MILLISECONDS);
     } catch (InterruptedException e) {
+      logger.info("futureGet interrupt, " + e.getMessage(), e);
       Thread.currentThread().interrupt();
-    } catch (ExecutionException ignored) {
+    } catch (ExecutionException e) {
+      logger.info("futureGet execute, " + e.getMessage(), e);
     } catch (TimeoutException e) {
-      logger.info("futureGet time out");
+      logger.info("futureGet timeout, " + e.getMessage(), e);
     }
 
     return null;
   }
 
   public void futureGet(Runnable runnable) {
-    ListenableFuture<?> future = executorService.submit(() -> {
+    Future<?> future = executorService.submit(() -> {
       try {
         dbManager.setMode(false);
         runnable.run();
       } catch (Exception e) {
-        logger.info("futureGet " + e.getMessage());
+        logger.info("futureGet " + e.getMessage(), e);
       }
     });
 
     try {
-      future.get(1000, TimeUnit.MILLISECONDS);
+      future.get(10000, TimeUnit.MILLISECONDS);
     } catch (InterruptedException e) {
+      logger.info("futureGet interrupt, " + e.getMessage(), e);
       Thread.currentThread().interrupt();
-    } catch (ExecutionException ignored) {
+    } catch (ExecutionException e) {
+      logger.info("futureGet execute, " + e.getMessage(), e);
     } catch (TimeoutException e) {
-      logger.info("futureGet time out");
+      logger.info("futureGet time out, " + e.getMessage(), e);
     }
   }
 }
